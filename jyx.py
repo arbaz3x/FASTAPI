17c0c324d0ab52c5619fe93ee32a2a8614440413
arbaz3x <arbazz.arshad10@gmail.com>
2024-05-04 19:57:38 +0500

    commit
    new changes

--- jyx.py
+++ jyx.py
@@ -1,1 +1,11 @@
-print('hi')
+from fastapi import FastAPI
+
+app = FastAPI() #created an instance of FAST-API Class
+
+@app.get("/") #route
+def response():
+    return {"Hi from fast api"}
+
+@app.get("/help") #route
+def new_response():
+    return {'Hi how can we help you?'}
